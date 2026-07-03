0a4ffbdeb6d7fcd72cbf3e5ea4ed3212c5779421
pratik <paithankarpratik3@gmail.com>
2026-07-03 09:16:10 +0530

    fixed the issue of rendering and now working fine

--- blog-app/src/app/blog/blog-list/blog-list.ts
+++ blog-app/src/app/blog/blog-list/blog-list.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Blog } from '../../services/blog';
 
 @Component({
   selector: 'app-blog-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './blog-list.html',
   styleUrl: './blog-list.css',
 })
-export class BlogList {}
+export class BlogList implements OnInit {
+
+  blogs: any[] = [];
+
+  private blogService = inject(Blog);
+  private cdr = inject(ChangeDetectorRef);
+
+  ngOnInit(): void {
+    this.loadBlogs();
+  }
+
+  private loadBlogs(): void {
+
+    this.blogService.getBlogs().subscribe({
+
+      next: (res: any) => {
+
+        console.log("Response:", res);
+
+        this.blogs = [...res];
+
+        console.log("Blogs:", this.blogs);
+
+        this.cdr.detectChanges();
+
+      },
+
+      error: (err) => {
+        console.error(err);
+      }
+
+    });
+  }
+}
+
